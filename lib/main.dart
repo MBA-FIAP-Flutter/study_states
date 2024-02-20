e2deafbd03de5ac92455bb1b88466bc1243f8303
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2024-02-20 09:32:14 -0300

    improvments

--- lib/main.dart
+++ lib/main.dart
@@ -22,26 +22,20 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Contador"),
+        title: const Text("Counter"),
       ),
       body:  Center(
         child: Column(
